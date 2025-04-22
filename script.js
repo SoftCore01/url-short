26b41751313511c6d5595c4b4a0b5017217aba86
SoftCore <soft0core1@gmail.com>
2025-04-22 10:46:50 +0100

    complete 2

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ let navEl = document.querySelector("nav");
 
 burgerEl.addEventListener("click", (e)=>{
     if (navEl.style.display != "block") {
-        console.log(navEl.style.display)
+        
         navEl.style.display = "block"
     } else {
-        console.log(navEl.style.display);
+        
         navEl.style.display = "none";
     }
 })
 
 window.addEventListener("resize", e => {
     if (window.screen.width > 768 && navEl.style.display == "none") {
-      console.log("hello");
+      
       navEl.style.display = "flex";
     } else if (window.screen.width < 768 && navEl.style.display == "flex") {
-      console.log("small")
+      
       navEl.style.display = "none";
     }
 })
